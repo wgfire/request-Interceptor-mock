fe545dcd8752efbe9f7cb5fa3e970a65bbce56fb
王港 <wangg11@mingyuanyu.com>
2022-02-18 18:07:53 +0800

    feat(pagescript): 调试拦截逻辑

--- src/pageScript/index/xhr.ts
+++ src/pageScript/index/xhr.ts
@@ -6,57 +6,66 @@
 // @ts-nocheck
 interface originalXhrInterface extends XMLHttpRequest {}
 const originXhr = new window.XMLHttpRequest();
-const Overload:XMLHttpRequest = {
-    
-    onreadystatechange (ev: XMLHttpRequestEventMap['readystatechange']) {
+const Overload: XMLHttpRequest = {
+    onreadystatechange(ev: XMLHttpRequestEventMap['readystatechange']) {
         console.log('状态改变', this.readyState, this.responseURL);
         if (this.readyState == 4) {
             console.log('请求完成');
-        } 
+        }
     },
     send(body?: Document | XMLHttpRequestBodyInit | null): void {
         console.log(body, 'body');
-      
     },
     setRequestHeader(name: string, value: string): void {
         console.log(name, value);
-    }
-}
-class Xhr  {
-      constructor () {
-        console.log(this)
-        // for(let selfAttr in Overload) {
-        //     console.log(selfAttr,'self')
-        //     originXhr[selfAttr] = Overload[selfAttr].bind(originXhr)
-        // }
-      
-        for (let attr in originXhr) {
-            if(attr==='onreadystatechange'){
-                originXhr[attr] = this.onreadystatechange
-            }
-            if(attr==='send'){
-                originXhr[attr] = this.send
-            }
-          
+    },
+};
+// class Xhr  {
+//       constructor () {
+//         console.log(this)
+//         // for(let selfAttr in Overload) {
+//         //     console.log(selfAttr,'self')
+//         //     originXhr[selfAttr] = Overload[selfAttr].bind(originXhr)
+//         // }
 
-            if (typeof originXhr[attr] === 'function') {
-                this[attr] = originXhr[attr].bind(originXhr);
-            } else {
-                this[attr] = originXhr[attr];
-            }
-        }
-    
-    }
-    onreadystatechange (ev: XMLHttpRequestEventMap['readystatechange']) {
-        console.log('状态改变', this.readyState, this.responseURL);
-        if (this.readyState == 4) {
-            console.log('请求完成');
-        } 
-    }
-    send(body?: Document | XMLHttpRequestBodyInit | null): void {
-        console.log(body, 'body');
+//         for (let attr in originXhr) {
+//             if(attr==='onreadystatechange'){
+//                 originXhr[attr] = this.onreadystatechange
+//             }
 
-    }
-}
+//             if (typeof originXhr[attr] === 'function') {
+//                 this[attr] = originXhr[attr].bind(originXhr);
+//             } else {
+//                 this[attr] = originXhr[attr];
+//             }
+//         }
 
-window.XMLHttpRequest = Xhr;
+//     }
+//     onreadystatechange (ev: XMLHttpRequestEventMap['readystatechange']) {
+//         console.log('状态改变', this.readyState, this.responseURL,this.responseText);
+//         if (this.readyState == 4) {
+//             console.log('请求完成');
+//         }
+//     }
+
+// }
+
+var req = new XMLHttpRequest();
+// req.prototype.open = function (method, url) {
+//     if (url) {
+//         // 成立的情况，原样发送
+//         console.log('拦截');
+
+//         req.prototype.open = xhr.open;
+//         xhr.open(method, url);
+//     } else {
+//         // 不成立的时候，进行拦截处理
+//         //  处理函数
+//     }
+// };
+window.XMLHttpRequest.prototype.open = function (method, url) {
+    console.log(method, 'x');
+  //  req.open(method, url);
+};
+
+// window.XMLHttpRequest = Xhr;
