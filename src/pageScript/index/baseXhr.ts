2dc3761780a4eb4921bc2ef6587ed74ab3557898
王港 <364049242@qq.com>
2022-03-09 20:52:48 +0800

    能够修改请求头信息

--- src/pageScript/index/baseXhr.ts
+++ src/pageScript/index/baseXhr.ts
@@ -44,5 +44,115 @@ export class BaseXhr {
     unset() {
         window.XMLHttpRequest = this.originXhr;
     }
+    /**
+     * 初始化 重写xhr对象
+     */
+     init() {
+        let _this = this;
+        //@ts-ignore
+        window.XMLHttpRequest = function () {
+            //@ts-ignore
+            this._xhr = new _this.originXhr();
+            // 用户new XMLHttpRequest创建的为当前this，此时将这个this上的属性进行代理
+            console.log(this, '用户的实例');
+            _this.overwrite(this);
+        };
+    }
+    /**
+     * 处理重写
+     * @param {*} xhr
+     */
+    overwrite(proxyXHR: any) {
+        for (let key in proxyXHR._xhr) {
+            if (typeof proxyXHR._xhr[key] === 'function') {
+                this.overwriteMethod(key, proxyXHR);
+                continue;
+            }
 
+            this.overwriteAttributes(key, proxyXHR);
+        }
+    }
+
+    /**
+     * 重写方法
+     * @param {*} key
+     */
+    overwriteMethod(key: string, proxyXHR: any) {
+        let hooks = this.hooks;
+        proxyXHR[key] = (...args: any[]) => {
+            // 拦截的方法
+            if (hooks[key] && hooks[key].call(proxyXHR, args) === false) {
+                return;
+            }
+            //console.log(key,'方法',args)
+
+            // 执行方法本体
+            const res = proxyXHR._xhr[key].apply(proxyXHR._xhr, args);
+
+            // 方法本体执行后的钩子
+
+            return res;
+        };
+    }
+
+    /**
+     * 重写属性
+     * @param {*} key
+     */
+    overwriteAttributes(key: string, proxyXHR: any) {
+        this.porxyOnHandel(key, proxyXHR);
+        Object.defineProperty(proxyXHR, key, this.setProperyDescriptor(key, proxyXHR));
+    }
+
+    /**
+     * 对_xhr为on开头的监听属性，赋值hooks下的代理函数
+     * @param key
+     * @param proxyXHR
+     */
+    porxyOnHandel(key: string, proxyXHR: any) {
+        if (key.startsWith('on') && this.hooks[key]) {
+            proxyXHR._xhr[key] = this.hooks[key].bind(proxyXHR);
+        }
+    }
+
+    /**
+     * 设置属性的属性描述,如果用户set了on开头的属性，也可以执行hooks下的监听函数
+     * @param {*} key
+     */
+    setProperyDescriptor(key: string, proxyXHR: any) {
+        let obj = Object.create(null);
+        let _this = this;
+
+        // 对用户设置的属性，挂载一份到自己的实例上
+        obj.set = function (val: any) {
+            console.log(key, 'set属性',val); // 用户设置的属性将会在此拦截
+            // 如果不是on打头的属性
+            if (!key.startsWith('on')) {
+                proxyXHR['__' + key] = val;
+                return;
+            }
+            // on开头监听函数 是null ,这里将实例的函数执行变为用户的hooks里的同名函数
+            if (_this.hooks[key]) {
+                console.log('拦截on的监听函数');
+                this._xhr[key] = function (...args: any[]) {
+                    _this.hooks[key].call(proxyXHR); // 执行代理函数
+                    val.apply(proxyXHR, args); // 执行用户set的函数
+                };
+
+                return;
+            }
+            // 其他的属性
+
+            this._xhr[key] = val;
+        };
+
+        obj.get = function () {
+            const result = proxyXHR['__' + key] || this._xhr[key];
+            console.log(key, 'get属性', proxyXHR['__' + key], this._xhr[key]);
+            // 此时用户访问这个对象上的其他属性通过拦截get 返回_xhr原生上的属性
+            return result;
+        };
+
+        return obj;
+    }
 }
--- src/pageScript/index/message.ts
+++ src/pageScript/index/message.ts
@@ -5,62 +5,85 @@ export const MockUrl = [
         statu: 200, // 状态
         switch: true, // 是否开启拦截
         cancel: false, // 是否取消请求
-        url: 'https://api-tools-test.mycaigou.com/tenant/tenant-list',
+        url: 'https://api-tools-test.mycaigou.com/message/erp-template-list?o=gyltest',
         request: {
             // 请求携带的信息
-            headers: [{}],
+            headers: [],
             timeout: 2000,
             data: {}, // 请求携带的数据
         },
         response: {
             data: {
-                bid_system_version_note: [
-                    {
-                        erp_version: 'v4.0',
-                        version_name: '采招ERP4.0',
-                    },
-                    {
-                        erp_version: 'v3.5',
-                        version_name: '采招ERP3.5',
-                    },
-                    '…',
-                ],
-                list: [
-                    {
-                        tenant_id: 'cz_60c022e3e01c2',
-                        tenant_code: 'cqdydc',
-                        portal_type: 'new',
-                        tenant_name: '重庆东原地产MOCK',
-                        bid_app_code: 'cgbid',
-                        bid_system_version: 'v3.5',
-                        bid_lease_start_time: '2021-06-08',
-                        bid_lease_end_time: '2021-07-08',
-                        bid_is_enable: '1',
-                        bid_isbuyout: '1',
-                        material_app_code: 'material',
-                        material_system_version: 'v3.5',
-                        material_lease_start_time: '2021-06-08',
-                        material_lease_end_time: '2021-07-08',
-                        material_is_enable: '0',
-                        material_isbuyout: '1',
-                        material_left_time: '已禁用',
-                        bid_left_time: '买断',
-                        address_url: 'https://home-test.myyscm.com/cqdydc',
-                        app_type: ['material', 'cgbid'],
-                        label_list: [
-                            {
-                                note_id: '201d7cee-c5aa-11eb-b960-6c92bf475505',
-                                note_name: '多租户(主)',
-                                tenant_id: 'cz_60c022e3e01c2',
-                            },
-                        ],
-                        cgbid_manager_info: null,
-                        material_manager_info: null,
-                    },
-                ],
-                total: '365',
+                list:[{
+                    "id": "3a0209d9-ce46-b8f8-3214-25830e3b0fc6",
+                    "business_type": "1",
+                    "title": "test",
+                    "msg_code": "test-zq",
+                    "erp_version": "default",
+                    "site_template_id": "t-site-test-zq",
+                    "sms_template_id": "t-sms-test-zq",
+                    "email_template_id": "",
+                    "ycg_wechat_template_id": "",
+                    "remark": "tsest",
+                    "created_by": "王港mock",
+                    "created_time": "2022-02-14 18:00:14",
+                    "modified_by": null,
+                    "modified_time": "2022-02-14 18:00:14",
+                    "is_deleted": "0",
+                    "message_type_code": "1",
+                    "is_edit": true
+                }],
+                total: "95"
             },
-            msg: '',
+       
+            msg: '操作成功',
+            status: true,
+        },
+    },
+    {
+        statu: 200, // 状态
+        switch: true, // 是否开启拦截
+        cancel: false, // 是否取消请求
+        url: 'https://api-tools-test.mycaigou.com/message/erp-record-list?o=gyltest',
+        request: {
+            // 请求携带的信息
+            headers: [],
+            timeout: 2000,
+            data: {}, // 请求携带的数据
+        },
+        response: {
+            data: {
+                list:[{
+                    "id": "133",
+                    "tenant_id": "cz_607e37c4c8637",
+                    "msg_code": "test-zq",
+                    "title": "test",
+                    "sms_template_id": "t-sms-test-zq",
+                    "sms_content": "asdf",
+                    "site_template_id": "t-site-test-zq",
+                    "site_content": "asdf",
+                    "site_message_type_code": "1",
+                    "detail_url": "",
+                    "detail_target_menu": "",
+                    "wechat_template_id": null,
+                    "wechat_content": null,
+                    "wechat_remark": null,
+                    "apply_reason": "tsest",
+                    "reject_reason": "",
+                    "from": "portal",
+                    "status": "2",
+                    "verify_by": "陈思思mock",
+                    "verify_on": "2022-02-14 18:00:14",
+                    "created_by": "mock",
+                    "created_on": "2021-12-20 15:31:04",
+                    "modified_by": "39fc28af-e1f4-4ae6-4861-ebca84bc04b6",
+                    "modified_on": "2021-12-20 15:31:04",
+                    "is_deleted": "0"
+                }],
+                total: "27"
+            },
+       
+            msg: '操作成功',
             status: true,
         },
     },
